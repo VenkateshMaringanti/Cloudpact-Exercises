33462dad5f6caad03a8c4dd275df5e012863fb5c
Venkatesh <venkatesh.maringanti@cloudpact.com>
2015-06-23 15:26:44 +0530

    Object Implementation

--- Mouse Events/mouseeventsmul.js
+++ Mouse Events/mouseeventsmul.js
@@ -1,3 +1,4 @@
+var msgDiv = document.getElementById("logMsg");
 function loadComplete(){
     var btn = document.getElementsByClassName("bt");
     var b = new Array();
@@ -13,8 +14,67 @@ function object(el){
     this.el = el;
 }
 
+object.prototype.onDown = function(e){
+    var that = this;
+    this.el.onmouseover = function(){ 
+        that.mouseOvr();
+    }
+    this.el.onmousemove = function(){
+        that.mouseMve();
+    }
+    this.changeClass("pressed");
+    logMesage("Mouse Down");
+    this.el.onmouseup = function(){
+        that.mouseUp();
+    }
+    this.el.onclick = function(){
+        that.mouseClick();
+    }
+    this.el.ondblclick = function(){
+        that.dblClick();
+    }
+    this.el.onmouseout = function(){
+        that.mouseOut();
+    }
+}
+
+object.prototype.mouseMve = function(){
+    logMesage("Mouse Move");
+}
+object.prototype.mouseOvr = function(){
+    logMesage("Mouse Over");
+}
+object.prototype.mouseUp = function(){
+    this.changeClass("bt");
+    logMesage("Mouse Up");
+}
+object.prototype.mouseClick = function(){
+    this.el.onmouseover = null;
+    this.el.onmousemove = null;
+    this.el.onmouseup = null;
+    this.el.ondblclick = null;
+    this.el.oncontextmenu = null;
+    logMesage("Mouse Click");
+}
+object.prototype.dblClick = function(){
+    logMesage("Double Click");
+}
+object.prototype.mouseOut = function(){
+    this.changeClass("bt");
+    this.el.onmouseover = null;
+    this.el.onmousemove = null;
+    this.el.onmouseup = null;
+    this.el.ondblclick = null;
+    this.el.onmouseout = null;
+    logMesage("Mouse Out");
+}
+
+object.prototype.changeClass = function(classname){
+    this.el.className = classname;
+}
+
 object.prototype.callFunction = function(e){
-    e.onmousedown = function(){
+    /*e.onmousedown = function(){
         e.onmouseover = function(){
             logMesage("Mouse Over");
         }
@@ -47,11 +107,16 @@ object.prototype.callFunction = function(e){
             e.onmouseout = null;
             logMesage("Mouse Out");
         }
+    }*/
+    var that = this;
+    e.onmousedown = function(e){
+        that.onDown(e); // Object
     }
 }
 
 function logMesage(msg){
-    document.getElementById("log").innerHTML += msg + "<br>" ;
+    document.getElementById("log").innerHTML += msg + "<br>";
+    msgDiv.scrollTop += msgDiv.scrollHeight;
 }
 
 document.getElementById("cl").onclick = function(){
